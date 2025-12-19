d2e2c7a742992231cdeed137f5646de231ec8a37
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2025-12-19 17:59:21 +0530

    Refactor: separate client controllers from client routes

---
+++ backend/src/controllers/client.controller.js
@@ -0,0 +1,50 @@
+import { Client } from "../models/Client.model.js";
+
+export const addClient = async (req, res) => {
+    try {
+        const { image, name, description, designation } = req.body;
+
+        if (!image || !name || !description || !designation) {
+            return res.status(400).json({
+                message: 'All fields are required'
+            });
+        }
+
+        const client = await Client.create({
+            image,
+            name,
+            description,
+            designation
+        });
+
+        return res.status(201).json({
+            message: 'Client added successfully',
+            client
+        });
+
+    } 
+    catch (error) {
+        return res.status(500).json({
+            message: 'Server error',
+            error: error.message
+        });
+    }
+}
+
+
+export const getClients = async (req, res)=>{
+    try{
+        const clients = await Client.find();
+
+        return res.status(200).json({
+            message: "Clients fetched successfully.",
+            clients
+        })
+    } 
+    catch(error){
+        return res.status(500).json({
+            message: 'Server error',
+            error: error.message
+        })
+    }
+}
