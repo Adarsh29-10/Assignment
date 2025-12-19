9784a793a005f9538644a80abf648f8c570e9ccd
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2025-12-19 14:49:19 +0530

    Add connection of mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express, { json } from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import CONNECT_DB from './src/db/index.js'
+
+dotenv.config()
 
 const app = express();
-const port = 8000 
+const port = process.env.PORT || 8000  
 
 app.use(cors())
 app.use(express.json())
@@ -11,4 +15,12 @@ app.get('/', (req, res)=>{
     res.send('Shree Ganesh')
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+
+CONNECT_DB()
+.then(()=>{
+    app.listen(port, () => console.log(`Listening on port ${port}`))
+})
+.catch((error)=>{
+    console.log('MONGO DB CONNECTION FAILED!', error);
+})
+
