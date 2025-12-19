bf9e7bea729ac5b5cc7a43fd75ea7ce42c7eea13
Adarsh Raghuwanshi <aadiraghuwanshi29@gmail.com>
2025-12-19 18:03:48 +0530

    Refactor: separate contact controllers from contact routes

--- backend/src/routes/contact.routes.js
+++ backend/src/routes/contact.routes.js
@@ -1,55 +1,10 @@
 import express from 'express'
-import { Contact } from '../models/Contact.model.js';
+import { addContact, getContact } from '../controllers/contact.controller.js';
 
 const router = express.Router();
 
-router.post('/new', async (req, res) => {
+router.post('/', addContact);
 
-    try {
-        const { fullName, email, mobileNumber, city } = req.body;
-
-        if (!fullName || !email || !mobileNumber || !city) {
-            return res.status(400).json({
-                message: 'All fields are required'
-            });
-        }
-
-        const user = await Contact.create({
-            fullName,
-            email,
-            mobileNumber,
-            city
-        });
-
-        return res.status(201).json({
-            message: 'User added successfully',
-            user
-        });
-
-    } 
-    catch (error) {
-        return res.status(500).json({
-            message: 'Server error',
-            error: error.message
-        });
-    }
-});
-
-router.get('/', async (req, res)=>{
-    try{
-        const users = await Contact.find();
-
-        return res.status(200).json({
-            message: "Users fetched successfully.",
-            users
-        })
-    } 
-    catch(error){
-        return res.status(500).json({
-            message: 'Server error',
-            error: error.message
-        })
-    }
-})
+router.get('/', getContact)
 
 export default router
